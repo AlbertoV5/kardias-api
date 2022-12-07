efc225b7b178e572bea82d7b363ddf3fabc26e1a
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2022-12-06 20:10:29 -0600

    added CORS

--- app/main.py
+++ app/main.py
@@ -17,19 +17,13 @@ app = FastAPI(
     docs_url="/docs",
 )
 
-# origins = [
-#     "http://localhost",
-#     "http://localhost:8080",
-#     "http://localhost:3000",
-#     "http://127.0.0.1:5500",
-# ]
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 async_engine.begin()
 app.include_router(api.v1.router, prefix="/api/v1")
